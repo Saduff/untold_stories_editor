b5d0b3332be1c57896aa7a0a99896717e5191b5d
Saduff <Saduff3950@gmail.com>
2016-03-27 23:21:58 +0300

    Hotfix for v1.3

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
 var mainVM = new mainViewModel();
 
 $(document).ready(function() {
+	$("#templates-container").load("templates.html");
 	$('select').material_select();
 	ko.applyBindings(mainVM, document.getElementById('page-container'));
 });
